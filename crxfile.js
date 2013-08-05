3ade912740cb42ecc2ba8467dbc5dbfb2315ec7b
Matthew Gertner <matthew@salsitasoft.com>
2013-08-05 15:48:38 +0200

    Refactoring and added unit tests

--- crxfile.js
+++ crxfile.js
@@ -1,42 +1,98 @@
 (function() {
-	var crypto = require('crypto');
-  var _s = require('underscore.string');
+	var path = require('path'),
+    crypto = require('crypto'),
+    _s = require('underscore.string'),
+    Zip = require('node-zip');
+
+  var MESSAGE_PREFIX = '__MSG_',
+    DEFAULT_LOCALE = 'en';
+
+  function parseCRXHeader(data) {
+    var header = {};
+    header.magic = data.substr(0, 4);
+    header.version = readUint32FromString(data, 4);
+    header.publicKeyLength = readUint32FromString(data, 8);
+    header.signatureLength = readUint32FromString(data, 12);
+    header.publicKey = data.substr(16, header.publicKeyLength);
+    header.signature = data.substr(16 + header.publicKeyLength, header.signatureLength);
+    header.length = 16 + header.publicKeyLength + header.signatureLength;
+    return header;
+  }
+
+  function generateExtensionID() {
+    var sha256 = crypto.createHash('sha256');
+    sha256.update(publicKey, 'binary');
+    var sha = sha256.digest('hex').substr(0, 32);
+    var charCode = 'a'.charCodeAt(0);
+    var remapped = '';
+    for (var i=0; i<sha.length; i++) {
+      remapped += String.fromCharCode(parseInt(sha[i], 16) + charCode)[0];
+    }
+    return remapped;
+  }
+
+  function readUint32FromString(str, offset) {
+    return str.charCodeAt(offset) +
+      str.charCodeAt(offset+1) * 256 +
+      str.charCodeAt(offset+2) * (256+256) +
+      str.charCodeAt(offset+3) * (256*256*256);
+  }
 
   function CRXFile(data) {
     this._data = data;
+    this._header = null;
+    this._manifest = null;
+    this._extensionId = null;
+    this._locales = {};
   }
 
   CRXFile.prototype = {
-    parseCRXHeader: function() {
-      var header = {};
-      header.magic = this._data.substr(0, 4);
-      header.version = this._readUint32FromString(this._data, 4);
-      header.publicKeyLength = this._readUint32FromString(this._data, 8);
-      header.signatureLength = this._readUint32FromString(this._data, 12);
-      header.publicKey = this._data.substr(16, this.publicKeyLength);
-      header.signature = this._data.substr(16 + this.publicKeyLength, this.signatureLength);
-      header.length = 16 + header.publicKeyLength + header.signatureLength;
-      return header;
+    get header() {
+      return this._header;
     },
 
-    generateExtensionID: function(publicKey) {
-      var sha256 = crypto.createHash('sha256');
-      sha256.update(publicKey, 'binary');
-      var sha = sha256.digest('hex').substr(0, 32);
-      var charCode = 'a'.charCodeAt(0);
-      var remapped = '';
-      for (var i=0; i<sha.length; i++) {
-        remapped += String.fromCharCode(parseInt(sha[i], 16) + charCode)[0];
+    get manifest() {
+      return this._manifest;
+    },
+
+    localizeMessage: function(msg, locale) {
+      locale = locale || DEFAULT_LOCALE;
+      if (_s.startsWith(msg, MESSAGE_PREFIX)) {
+        return this._locales[locale][msg.substr(MESSAGE_PREFIX.length)].message;
       }
-      return remapped;
     },
 
-    getBestIcon: function(manifest, preferredSize) {
-      var icons = manifest.icons;
+    unpack: function(fileCallback, doneCallback) {
+      this._header = parseCRXHeader(this._data);
+      var zip = new Zip(this._data.substr(this._header.length));
+      var manifest;
+
+      for (var index in zip.files) {
+        var zipEntry = zip.files[index];
+        var filename = zipEntry.name;
+        fileCallback(filename, zipEntry.asBinary());
+        if ('manifest.json' === filename) {
+          this._manifest = JSON.parse(zipEntry.asText());
+        }
+        else if (_s.endsWith(filename, 'messages.json')) {
+          var segments = filename.split(path.sep);
+          if (3 === segments.length &&
+            '_locales' === segments[0] &&
+            'messages.json' === segments[2]) {
+            this._locales[segments[1]] = JSON.parse(zipEntry.asText());
+          }
+        }
+      }
+
+      doneCallback();
+    },
+
+    getBestIcon: function(preferredSize) {
+      var icons = this._manifest.icons;
       if (icons) {
         var bestSize = 0;
         // Simpleminded algorithm to get best icon size.
-        // Optimal is 48x48, otherwise use the largest one available.
+        // Optimal is exact match, otherwise use the largest one available.
         for (var sizeStr in icons) {
           var size = parseInt(sizeStr, 10);
           if (preferredSize === size) {
@@ -52,30 +108,6 @@
           return icons[bestSize];
         }
       }
-    },
-
-    unpack: function(ZipConstructor, fileCallback, doneCallback) {
-      var crxHeader = this.parseCRXHeader();
-      var zip = new ZipConstructor(this._data.substr(crxHeader.length));
-      var manifest;
-
-      for (var index in zip.files) {
-        var zipEntry = zip.files[index];
-        var filename = zipEntry.name;
-        fileCallback(filename, zipEntry.asBinary());
-        if ('manifest.json' === filename) {
-          manifest = zipEntry.asText();
-        }
-      }
-
-      doneCallback(crxHeader, JSON.parse(manifest));
-    },
-
-    _readUint32FromString: function(str, offset) {
-      return str.charCodeAt(offset) +
-        str.charCodeAt(offset+1) * 256 +
-        str.charCodeAt(offset+2) * (256+256) +
-        str.charCodeAt(offset+3) * (256*256*256);
     }
   };
 
